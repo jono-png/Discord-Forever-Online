2886c90d8b8b9f24146ae1ec028b61bbd06b9a18
jono <159429141+jono-png@users.noreply.github.com>
2024-05-16 12:49:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
 
-// Replace TOKEN with your bot account's token
-const bot = new Eris(process.env.token);
+const bot = new Eris("YOUR_BOT_TOKEN_HERE");
 
 bot.on("error", (err) => {
-  console.error(err); // or your preferred logger
+  console.error(err);
 });
 
-bot.connect(); // Get the bot to connect to Discord
+bot.connect();
